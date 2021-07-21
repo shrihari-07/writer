5f5e499eb29b1456ea2de67ffc5f3d2c4e0a1fd9
satishkumarsajjan <55579930+satishkumarsajjan@users.noreply.github.com>
2021-07-21 14:56:44 +0530

    Add files via upload
    added login stuff

---
+++ views/process_login.php
@@ -0,0 +1,22 @@
+ <?php
+    require('mysql_connection.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $userName = stripslashes($_REQUEST['username']);    // removes backslashes
+        $userName = mysqli_real_escape_string($con, $userName);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM users WHERE username='".$userName."'
+                     AND password='".$password."'";
+        $result = mysqli_query($con, $query);
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['username'] = $userName;
+            header("Location: home.php");
+        } else {
+            header("Location: login.php?login=error");
+        }
+    }
+?>
