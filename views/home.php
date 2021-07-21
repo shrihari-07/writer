832d0bddfb154df9d7553a1bcd4e024e33c6ec7f
Shrihari K <shrihari.kowthal@gmail.com>
2021-07-21 13:33:19 +0530

    Update files

--- views/home.php
+++ views/home.php
@@ -1,8 +1,49 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["user"])){
-        echo "<h1>404 Page not found</h1>";
-    } else {
-        echo "<h1>Hello World</h1>";
-    }
+session_start();
+if (!isset($_SESSION["user"])) {
+    echo "<h1>404 Page not found</h1>";
+} else {
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Google fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Akaya+Kanadaka&family=Montserrat&display=swap" rel="stylesheet" />
+
+        <!-- FontAwesome CDN -->
+        <script src="https://kit.fontawesome.com/c5891fcd06.js" crossorigin="anonymous"></script>
+
+        <!-- Link to home.css stylesheet -->
+        <link rel="stylesheet" href="../public/css/home.css">
+        <title>Writer | Home</title>
+    </head>
+
+    <body>
+        <div id="home">
+            <div class="home-nav">
+                <div class="logo">
+                    <a href="home.php">
+                        <h1>Writer</h1>
+                    </a>
+                </div>
+                <div class="home-nav-items">
+                    <a style="color: black;" href="home.php"><img src="../public/assets/home.png"></img></a>
+                    <a style="color: black;" href="user.php"><img src="../public/assets/user.png"></img></a>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    </html>
+
+<?php
+}
 ?>
